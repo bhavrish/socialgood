c87af69b747c8fc6c6884d871626affe3aa8d4e0
Bhavesh Shah <bhaveshshah@bhaveshs-air.dyndns.rice.edu>
2019-09-21 08:08:15 -0500

    added swift project

---
+++ socialgood_test/socialgood_testTests/socialgood_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  socialgood_testTests.swift
+//  socialgood_testTests
+//
+//  Created by Bhavesh Shah on 9/21/19.
+//  Copyright © 2019 Bhavesh Shah. All rights reserved.
+//
+
+import XCTest
+@testable import socialgood_test
+
+class socialgood_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
