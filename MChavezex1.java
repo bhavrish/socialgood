24583341009c0b8f2f4addf497cec9c9571637c9
mchavez28 <mc482102@tamu.edu>
2019-09-20 22:30:36 -0500

    added a file

---
+++ MChavezex1.java
@@ -0,0 +1,13 @@
+/*
+CSCE 111 501
+Mariah Chavez
+UIN 827003839
+9/3/2019
+exercise 1
+*/
+class MChavezex1 {
+  public static void main(String[] args) {
+  System.out.println("Hello World");
+  
+} //end main
+} // end class
